373f87a7d1fb3280c1092a14d8673b5d5fd8b248
cordis <cordis@game-mafia.ru>
2014-08-21 20:12:56 +0800

    f version fixed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pybeans',
-    version='0.10.0',
+    version='0.20.0',
     packages=find_packages(),
     author='CordiS',
     author_email='cordis@game-mafia.ru',
