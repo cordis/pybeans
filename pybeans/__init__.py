c85fae906fb9fbfc81e7b84979d986bd42675e8a
cordis <cordis@game-mafia.ru>
2015-11-05 12:55:11 +0700

    r __init__.py simplified

--- pybeans/__init__.py
+++ pybeans/__init__.py
@@ -1,43 +1,5 @@
-from pybeans.const import UNDEFINED
-from pybeans.schema import Schema
 from pybeans.exceptions import *
-
-
-def bean(_=None, **defaults):
-    def _bean(cls):
-        cls.__pybeansschema__ = Schema(cls.__dict__, defaults)
-        for name, node in cls.__pybeansschema__.get_nodes():
-            setattr(cls, name, NotImplemented)
-        return cls
-
-    if _ is not None:
-        return _bean(_)
-    return _bean
-
-
-def bean_to_dict(source, target=None):
-    """
-    @param source: bean object or class
-    @type target: C{dict}
-    @rtype: C{dict}
-    @returns: full filled target
-    @raises L{pybeans.exceptions.EncodingError}
-    """
-    if target is None:
-        target = {}
-    return source.__pybeansschema__.encode(source, target)
-
-
-def dict_to_bean(source, target):
-    """
-    @type source: C{dict}
-    @param target: bean object or class
-    @returns: full filled target
-    @raises L{pybeans.exceptions.EncodingError}
-    """
-    if isinstance(target, type):
-        target = target()
-    return target.__pybeansschema__.decode(source, target)
+from pybeans.shortcuts import *
 
 
 __all__ = [
---
+++ pybeans/shortcuts.py
@@ -0,0 +1,38 @@
+from pybeans.schema import Schema
+
+
+def bean(_=None, **defaults):
+    def _bean(cls):
+        cls.__pybeansschema__ = Schema(cls.__dict__, defaults)
+        for name, node in cls.__pybeansschema__.get_nodes():
+            setattr(cls, name, NotImplemented)
+        return cls
+
+    if _ is not None:
+        return _bean(_)
+    return _bean
+
+
+def bean_to_dict(source, target=None):
+    """
+    @param source: bean object or class
+    @type target: C{dict}
+    @rtype: C{dict}
+    @returns: full filled target
+    @raises L{pybeans.exceptions.EncodingError}
+    """
+    if target is None:
+        target = {}
+    return source.__pybeansschema__.encode(source, target)
+
+
+def dict_to_bean(source, target):
+    """
+    @type source: C{dict}
+    @param target: bean object or class
+    @returns: full filled target
+    @raises L{pybeans.exceptions.EncodingError}
+    """
+    if isinstance(target, type):
+        target = target()
+    return target.__pybeansschema__.decode(source, target)
